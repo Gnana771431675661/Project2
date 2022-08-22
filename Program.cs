d38b944d05541522b822fe702b5449bd63848eb1
KUPPILI GNANENDRA <80825319+Gnana771431675661@users.noreply.github.com>
2022-08-22 20:26:48 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+
+namespace convert_float_to_int
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float n;
+            Console.WriteLine("Enter Float value: ");
+            n = float.Parse(Console.ReadLine());
+            int i = (int)n;
+            Console.WriteLine("Converted float {0} to int {1}", n, i);
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # Project2
-Write a C# Program to enter input value as float and output value should be an Integer? 
